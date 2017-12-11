5777f79619fc22498b7a4779b6fa784ca49ae165
方永乐 <fangyongle@innobuddy.com>
2017-12-11 16:30:11 +0800

    add helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import reduxConnect from './reduxConnectDecorator';
 import withRouter from './withRouterDecorator';
 import fetchData from './fetchDataDecoretor';
+import { Helmet } from 'react-helmet';
 
 export default {
   reduxConnect,
   withRouter,
-  fetchData
+  fetchData,
+  Helmet
 };
